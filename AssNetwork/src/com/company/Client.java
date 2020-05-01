31511eff5e3234e9a8312c9914f75f6bb9758117
Aniket Powar <aniketpowar@192-168-1-105.tpgi.com.au>
2020-05-02 00:31:31 +1000

    new

--- AssNetwork/src/com/company/Client.java
+++ AssNetwork/src/com/company/Client.java
@@ -1,10 +1,15 @@
 package com.company;
 
 import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
@@ -30,11 +35,14 @@ public class Client {
         String DISCONNECT = "disconnect";
         Boolean flag = false;
         String requestType = null;
+        String temp = null;
         String target = null;
         String content = null;
         JSONObject jsonObjectGET = new JSONObject();
         JSONObject jsonObjectPUT = new JSONObject();
         JSONParser par = new JSONParser();
+        String fileRelativePath = (new File("").getAbsolutePath());
+        String pathOfFile = fileRelativePath + "/www";
 
         try {
             parser.parseArgument(args);
@@ -55,82 +63,136 @@ public class Client {
             Scanner cmdin = new Scanner(System.in);
             while (true) {
                 String msg = cmdin.nextLine();
-                System.out.println("test");
+                //System.out.println(msg);
                 // forcing TCP to send data immediately
                 StringTokenizer stringTokenizer = new StringTokenizer(msg, " ");
+                try {
                     while (stringTokenizer.hasMoreTokens()) {
-                        //System.out.println(stringTokenizer.nextToken());
-                        if (stringTokenizer.nextToken().equals(GET)) {
+                        String type = stringTokenizer.nextToken();
+                        if (type.equals(GET)) {
                             requestType = GET;
                             target = stringTokenizer.nextToken();
-                        }
-                        else if (stringTokenizer.nextToken().equals(PUT)) {
+                        } else if (type.equals(PUT)) {
                             requestType = PUT;
+                            temp = stringTokenizer.nextToken();
                             target = stringTokenizer.nextToken();
-                        }
-                        else if (stringTokenizer.nextToken().equals(DELETE)) {
+                        } else if (type.equals(DELETE)) {
                             requestType = DELETE;
                             target = stringTokenizer.nextToken();
-                        }
-                        else if (stringTokenizer.nextToken().equals(DISCONNECT)) {
+                        } else if (type.equals(DISCONNECT)) {
                             requestType = DISCONNECT;
-                            target = stringTokenizer.nextToken();
+                            //target = stringTokenizer.nextToken();
                         }
-
                     }
+                    if (requestType.equals(GET)) {
+                        jsonObjectGET.put("message", "request");
+                        jsonObjectGET.put("type", GET.toUpperCase());
+                        jsonObjectGET.put("target", target);
+                        out.write((String.valueOf(jsonObjectGET) + "\n").getBytes("UTF-8"));
+                        out.flush();
 
-                if (requestType.equals(GET)){
-                    System.out.println("Inside get");
-                    jsonObjectGET.put("message","request");
-                    jsonObjectGET.put("type", GET.toUpperCase());
-                    jsonObjectGET.put("target", target);
-                    out.write((String.valueOf(jsonObjectGET)+ "\n").getBytes("UTF-8"));
-                    out.flush();
+                        System.out.println("CLIENT reading data");
+                        int c;
+                        String msg1 = "";
+                        do {
+                            c = in.read();
+                            msg1 += (char) c;
+                        } while (in.available() > 0);
 
-                    System.out.println("CLIENT reading data");
+                        String[] str = msg1.split("}");
+                        String response = str[0] + "}";
+                        //System.out.println(msg1);
 
-                    int c;
-                    String msg1 = "";
-                    do {
-                        c = in.read();
-                        msg1+=(char)c;
-                    } while(in.available()>0);
+                        JSONObject jsonObject2 = (JSONObject) par.parse(response);
 
-                    String[] str = msg1.split("}");
-                    String response = str[0] + "}";
-                    //System.out.println(msg1);
+                        String reply = (String) jsonObject2.get("content");
+                        System.out.println(reply);
 
-                    JSONObject jsonObject2= (JSONObject) par.parse(response);
+                    } else if (requestType.equals(PUT)) {
+                        String getPath = pathOfFile + temp;
+                        boolean flag1 = Files.exists(Paths.get(getPath));
+                        if (flag1) {
+                            System.out.println("File exist");
+                            FileReader read = new FileReader(getPath);
+                            Scanner scanFile = new Scanner(read);
+                            String content1 = "";
+                            while (scanFile.hasNextLine()) {
+                                content1 = content1 + scanFile.nextLine();
+                            }
+                            System.out.println(content1);
+                            jsonObjectPUT.put("message", "request");
+                            jsonObjectPUT.put("type", PUT.toUpperCase());
+                            jsonObjectPUT.put("target", target);
+                            jsonObjectPUT.put("content", content1);
+                            out.write((String.valueOf(jsonObjectPUT) + "\n").getBytes("UTF-8"));
+                            out.flush();
+                            int c;
+                            String msg1 = "";
+                            do {
+                                c = in.read();
+                                msg1 += (char) c;
+                            } while (in.available() > 0);
+                            String[] tempStr = msg1.split("}");
+                            String serverReply = tempStr[0] + "}";
+                            //System.out.println(msg1);
+                            JSONObject jsonObject2 = (JSONObject) par.parse(serverReply);
+                            String reply = (String) jsonObject2.get("content");
+                            System.out.println(reply);
 
-                    String reply= (String) jsonObject2.get("content");
-                    System.out.println(reply);
 
-                }else if (requestType.equals(PUT)){
-                    System.out.println("Inside put");
+                        } else System.out.println("Source file not found.");
 
 
-//                    jsonObjectPUT.put("message","request");
-//                    jsonObjectPUT.put("type", PUT.toUpperCase());
-//                    jsonObjectPUT.put("target", target);
-//                    out.write((String.valueOf(jsonObjectPUT)+ "\n").getBytes("UTF-8"));
-//                    out.flush();
+                    } else if (requestType.equals(DELETE)) {
 
-                    //TODO
-                }else if(requestType.equals(DELETE)){
-                    //TODO
-                }else if(requestType.equals(DISCONNECT)){
-                    //TODO
-                }
-                else {
-                    break;
+                        jsonObjectGET.put("message", "request");
+                        jsonObjectGET.put("type", DELETE.toUpperCase());
+                        jsonObjectGET.put("target", target);
+                        out.write((String.valueOf(jsonObjectGET) + "\n").getBytes("UTF-8"));
+                        out.flush();
+
+                        int c;
+                        String msg1 = "";
+                        do {
+                            c = in.read();
+                            msg1 += (char) c;
+                        } while (in.available() > 0);
+                        String[] tempStr = msg1.split("}");
+                        String serverReply = tempStr[0] + "}";
+                        //System.out.println(msg1);
+                        JSONObject jsonObject2 = (JSONObject) par.parse(serverReply);
+                        String reply = (String) jsonObject2.get("content");
+                        System.out.println(reply);
+
+                    } else if (requestType.equals(DISCONNECT)) {
+
+
+                        jsonObjectGET.put("message", "request");
+                        jsonObjectGET.put("type", DISCONNECT.toUpperCase());
+                        System.out.println("Successfully disconnected");
+                        out.write((String.valueOf(jsonObjectGET) + "\n").getBytes("UTF-8"));
+                        out.flush();
+//                    socket.shutdownInput();
+//                    socket.shutdownOutput();
+                        in.close();
+                        out.close();
+                        socket.close();
+                    } else {
+                        break;
+                    }
+                }catch (NoSuchElementException e) {
+                    continue;
                 }
             }
 
             cmdin.close();
+
             in.close();
             out.close();
             socket.close();
 
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         } catch (CmdLineException e) {
